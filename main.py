94f5cecae269db8df421f23674cd79b5b6c21afa
Yuns <87447660+0xUNS@users.noreply.github.com>
2022-10-20 04:13:02 +0000

    Create a user dashboard with key metrics

--- main.py
+++ main.py
@@ -138,6 +138,7 @@ factorial = math.factorial(5)
 print(f"\nSquare root of 16: {square_root}")
 print(f"2 raised to the power of 3: {power}")
 print(f"Factorial of 5: {factorial}")
+# New line 5436
 
 # Define a constant
 MY_CONSTANT = 3.14159
