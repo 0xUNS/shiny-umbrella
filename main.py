d69bfbf8cb67ae75ccda65ca9b80446587dc9aa9
Yuns <87447660+0xUNS@users.noreply.github.com>
2023-01-19 16:55:38 +0000

    Implement a feature to allow users to create and manage discussions

--- main.py
+++ main.py
@@ -124,7 +124,6 @@ print(f"\nAddition: {addition}")
 print(f"Subtraction: {subtraction}")
 print(f"Multiplication: {multiplication}")
 print(f"Division: {division}")
-
 # Use the math module to perform some more calculations
 square_root = math.sqrt(16)
 power = math.pow(2, 3)
