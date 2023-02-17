41c0a10b61416c1b077652abf3d275c5930a250e
Yuns <87447660+0xUNS@users.noreply.github.com>
2023-02-17 06:30:59 +0000

    Add a feature to allow users to view their login history

--- main.py
+++ main.py
@@ -14,11 +14,9 @@ random_strings = [generate_random_string(10) for _ in range(10)]
 for i, rs in enumerate(random_strings, start=1):
     print(f"Random string {i}: {rs}")
 
-# New line 8600
 # Define a class for a simple calculator
 class SimpleCalculator:
     def __init__(self, a, b):
-# New line 9182
         self.a = a
         self.b = b
 
@@ -26,13 +24,13 @@ class SimpleCalculator:
         return self.a + self.b
 
     def subtract(self):
-# New line 8455
         return self.a - self.b
 
     def multiply(self):
         return self.a * self.b
 
     def divide(self):
+        return self.a / self.b
 
 # Create an instance of the SimpleCalculator class
 calc = SimpleCalculator(10, 5)
@@ -74,6 +72,7 @@ while count < 100:
 
 Sure, here's an updated version of the script with additional code:
 
+```python
 # main.py
 
 # Import necessary modules
@@ -94,7 +93,6 @@ for i, rs in enumerate(random_strings, start=1):
     print(f"Random string {i}: {rs}")
 
 # Define a class for a simple calculator
-# New line 4054
 class SimpleCalculator:
     def __init__(self, a, b):
         self.a = a
@@ -110,8 +108,6 @@ class SimpleCalculator:
         return self.a * self.b
 
     def divide(self):
-# New line 2992
-# New line 5337
         return self.a / self.b
 
 # Create an instance of the SimpleCalculator class
@@ -123,10 +119,12 @@ subtraction = calc.subtract()
 multiplication = calc.multiply()
 division = calc.divide()
 
+# Print the results
 print(f"\nAddition: {addition}")
 print(f"Subtraction: {subtraction}")
 print(f"Multiplication: {multiplication}")
 print(f"Division: {division}")
+
 # Use the math module to perform some more calculations
 square_root = math.sqrt(16)
 power = math.pow(2, 3)
@@ -134,8 +132,10 @@ factorial = math.factorial(5)
 
 # Print the results
 print(f"\nSquare root of 16: {square_root}")
+print(f"2 raised to the power of 3: {power}")
 print(f"Factorial of 5: {factorial}")
 
+# Define a constant
 MY_CONSTANT = 3.14159
 
 # Create a dictionary with some key-value pairs
@@ -160,6 +160,7 @@ while count < 100:
 
 # Use a for loop to iterate over a range of numbers
 for num in range(1, 11):
+    print(num)
 
 # Use a list comprehension to generate a list of squares
 squares = [x**2 for x in range(1, 11)]
@@ -167,14 +168,13 @@ print("\nList of squares:", squares)
 
 # Use a lambda function to sort a list of numbers
 numbers = [3, 1, 4, 1, 5, 9, 2]
-# New line 6667
 sorted_numbers = sorted(numbers, key=lambda x: x % 2)
 print("\nSorted numbers:", sorted_numbers)
 
+# Define a class for a simple bank account
 class BankAccount:
     def __init__(self, balance=0):
         self.balance = balance
-
     def deposit(self, amount):
         self.balance += amount
         return self.balance
