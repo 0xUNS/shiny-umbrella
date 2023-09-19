bd9eb41e1f6f698c61b0b772654fd517720e5a83
Yuns <87447660+0xUNS@users.noreply.github.com>
2023-09-19 18:31:15 +0000

    Create a user onboarding process

--- main.py
+++ main.py
@@ -85,7 +85,6 @@ import string
 import math
 
 # Define a function to generate a random string
-def generate_random_string(length):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
 
