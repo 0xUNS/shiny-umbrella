00f2df9dafc398e57facd0f36850f8a2f06d08dd
Yuns <87447660+0xUNS@users.noreply.github.com>
2023-10-03 23:45:39 +0000

    Implement a feedback form for user suggestions

---
+++ main.py
@@ -0,0 +1,192 @@
+# Import necessary modules
+import random
+import string
+
+# Define a function to generate a random string
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+# Generate a list of 10 random strings
+random_strings = [generate_random_string(10) for _ in range(10)]
+
+# Print the list of random strings
+for i, rs in enumerate(random_strings, start=1):
+    print(f"Random string {i}: {rs}")
+
+# Define a class for a simple calculator
+class SimpleCalculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+    def subtract(self):
+        return self.a - self.b
+
+    def multiply(self):
+        return self.a * self.b
+
+    def divide(self):
+        return self.a / self.b
+
+# Create an instance of the SimpleCalculator class
+calc = SimpleCalculator(10, 5)
+
+# Perform some calculations
+addition = calc.add()
+subtraction = calc.subtract()
+multiplication = calc.multiply()
+division = calc.divide()
+
+# Print the results
+print(f"\nAddition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
+
+# Define a constant
+MY_CONSTANT = 3.14159
+
+# Create a dictionary with some key-value pairs
+my_dict = {"apple": 1, "banana": 2, "cherry": 3}
+
+# Iterate over the dictionary and print the keys and values
+for key, value in my_dict.items():
+    print(f"Key: {key}, Value: {value}")
+
+# Use a try-except block to handle a potential error
+try:
+    # Attempt to divide by zero
+    result = 1 / 0
+except ZeroDivisionError:
+
+# Use a while loop to count up to 100
+count = 0
+while count < 100:
+    print(count)
+    count += 1
+
+Sure, here's an updated version of the script with additional code:
+
+```python
+# main.py
+
+# Import necessary modules
+import random
+import string
+import math
+
+# Define a function to generate a random string
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+# Generate a list of 10 random strings
+random_strings = [generate_random_string(10) for _ in range(10)]
+
+# Print the list of random strings
+for i, rs in enumerate(random_strings, start=1):
+    print(f"Random string {i}: {rs}")
+
+# Define a class for a simple calculator
+class SimpleCalculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+    def subtract(self):
+        return self.a - self.b
+
+    def multiply(self):
+        return self.a * self.b
+
+    def divide(self):
+        return self.a / self.b
+
+# Create an instance of the SimpleCalculator class
+calc = SimpleCalculator(10, 5)
+
+# Perform some calculations
+addition = calc.add()
+subtraction = calc.subtract()
+multiplication = calc.multiply()
+division = calc.divide()
+
+# Print the results
+print(f"\nAddition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
+
+# Use the math module to perform some more calculations
+square_root = math.sqrt(16)
+power = math.pow(2, 3)
+factorial = math.factorial(5)
+
+# Print the results
+print(f"\nSquare root of 16: {square_root}")
+print(f"2 raised to the power of 3: {power}")
+print(f"Factorial of 5: {factorial}")
+
+# Define a constant
+MY_CONSTANT = 3.14159
+
+# Create a dictionary with some key-value pairs
+my_dict = {"apple": 1, "banana": 2, "cherry": 3}
+
+# Iterate over the dictionary and print the keys and values
+for key, value in my_dict.items():
+    print(f"Key: {key}, Value: {value}")
+
+# Use a try-except block to handle a potential error
+try:
+    # Attempt to divide by zero
+    result = 1 / 0
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+
+# Use a while loop to count up to 100
+count = 0
+while count < 100:
+    print(count)
+    count += 1
+
+# Use a for loop to iterate over a range of numbers
+for num in range(1, 11):
+    print(num)
+
+# Use a list comprehension to generate a list of squares
+squares = [x**2 for x in range(1, 11)]
+print("\nList of squares:", squares)
+
+# Use a lambda function to sort a list of numbers
+numbers = [3, 1, 4, 1, 5, 9, 2]
+sorted_numbers = sorted(numbers, key=lambda x: x % 2)
+print("\nSorted numbers:", sorted_numbers)
+
+# Define a class for a simple bank account
+class BankAccount:
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return "Insufficient funds"
+        self.balance -= amount
+        return self.balance
+
+# Create an instance of the BankAccount class
+account = BankAccount(1000)
+
+# Make a deposit
+deposit_amount = account.deposit(500)
