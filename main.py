cf89a5a74656617ffa39c1fe0fab88b21ab9f5ac
Yuns <87447660+0xUNS@users.noreply.github.com>
2024-02-14 21:52:48 +0000

    Refactor front-end code for better modularity

--- main.py
+++ main.py
@@ -125,6 +125,7 @@ division = calc.divide()
 print(f"\nAddition: {addition}")
 print(f"Subtraction: {subtraction}")
 print(f"Multiplication: {multiplication}")
+# New line 8050
 print(f"Division: {division}")
 
 # Use the math module to perform some more calculations
