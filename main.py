314476b3ff8f233150b4f0fa21c60eeca0a344c9
Yuns <87447660+0xUNS@users.noreply.github.com>
2024-03-26 15:25:52 +0000

    Fix issue with user login not redirecting correctly

--- main.py
+++ main.py
@@ -10,20 +10,22 @@ def generate_random_string(length):
 # Generate a list of 10 random strings
 random_strings = [generate_random_string(10) for _ in range(10)]
 
+# Print the list of random strings
 for i, rs in enumerate(random_strings, start=1):
     print(f"Random string {i}: {rs}")
 
 # Define a class for a simple calculator
+class SimpleCalculator:
     def __init__(self, a, b):
         self.a = a
+        self.b = b
 
     def add(self):
         return self.a + self.b
-# New line 2629
 
+# New line 5996
     def subtract(self):
         return self.a - self.b
-# New line 7898
 
     def multiply(self):
         return self.a * self.b
@@ -35,7 +37,6 @@ for i, rs in enumerate(random_strings, start=1):
 calc = SimpleCalculator(10, 5)
 
 # Perform some calculations
-# New line 1804
 addition = calc.add()
 subtraction = calc.subtract()
 multiplication = calc.multiply()
@@ -78,6 +79,7 @@ Sure, here's an updated version of the script with additional code:
 # Import necessary modules
 import random
 import string
+import math
 
 # Define a function to generate a random string
 def generate_random_string(length):
@@ -126,7 +128,6 @@ print(f"Division: {division}")
 
 # Use the math module to perform some more calculations
 square_root = math.sqrt(16)
-# New line 9553
 power = math.pow(2, 3)
 factorial = math.factorial(5)
 
@@ -167,14 +168,18 @@ squares = [x**2 for x in range(1, 11)]
 print("\nList of squares:", squares)
 
 # Use a lambda function to sort a list of numbers
+numbers = [3, 1, 4, 1, 5, 9, 2]
 sorted_numbers = sorted(numbers, key=lambda x: x % 2)
 print("\nSorted numbers:", sorted_numbers)
 
 # Define a class for a simple bank account
 class BankAccount:
     def __init__(self, balance=0):
+        self.balance = balance
+
     def deposit(self, amount):
         self.balance += amount
+        return self.balance
 
     def withdraw(self, amount):
         if amount > self.balance:
