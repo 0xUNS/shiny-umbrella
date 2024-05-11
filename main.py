a7e3a7a333a25d743797301155c5e0aac8eb649e
Yuns <87447660+0xUNS@users.noreply.github.com>
2024-05-11 12:40:41 +0000

    Implement a feature to allow users to follow other users' activities

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ class SimpleCalculator:
     def divide(self):
         return self.a / self.b
 
-# Create an instance of the SimpleCalculator class
 calc = SimpleCalculator(10, 5)
 
 # Perform some calculations
