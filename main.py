a6abbc052bdd5b84411348cfb07c5ddc2f690ac5
Yuns <87447660+0xUNS@users.noreply.github.com>
2024-08-14 15:40:44 +0000

    Create a script for data import from CSV

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ class SimpleCalculator:
 
     def divide(self):
         return self.a / self.b
+# New line 9378
 
 # Create an instance of the SimpleCalculator class
 calc = SimpleCalculator(10, 5)
